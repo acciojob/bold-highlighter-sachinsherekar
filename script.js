3de813840527eb836a0236e9a5cd40139fcdd203
sachinsherekar <130824306+sachinsherekar@users.noreply.github.com>
2023-06-07 15:39:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-function highlight() {
-    //Write your code here
-
-
+ //Write your code here
+	var bold=document.getElementsByTagName("strong");
+	for(let i=0;i<bold.length;i++){
+		bold[i].classList.add("highlight");
+	}
 }
-
-
 function return_normal() {
     //Write your code here
-
-    
-}
+	var bold=document.getElementsByTagName("strong");
+     for(let i=0;i<bold.length;i++){
+		 bold[i].classList.remove("highlight");}
